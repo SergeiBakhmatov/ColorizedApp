2a04145e11dc32d18d5d86b2e031fbad5ce9d6a8
Sergei Bakhmatov <94bahmatov@gmail.com>
2023-04-12 07:21:00 +0500

    Preparation for work

---
+++ HW 2/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  HW 2
+//
+//  Created by Sergei Bakhmatov on 12.04.2023.
+//  Copyright © 2023 Alexey Efimov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+
+
+
+}
